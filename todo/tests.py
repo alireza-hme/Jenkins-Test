6d8fc040c3ae04bb154ed9ebe6673d24e6914951
alireza-hme <hme.alireza@gmail.com>
2025-05-25 22:44:56 +0330

    minor changes

--- todo/tests.py
+++ todo/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from rest_framework.test import APIClient
 from .models import ToDo
 
-
 class ToDoAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
